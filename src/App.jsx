2342e7708c06ed37ddb84ab365c30b6989cadbc4
антон <kin.kin.2013@mail.ru>
2024-08-22 18:36:57 +0400

    first comit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
-import './App.css'
-import { Input } from './components/Input'
-
-export const  App = () =>  {
-  return (
-      <div>
-          <Input/>  
-        <h1>пивет</h1>
-      </div>
-    
-  )
+import { Input } from "./components/Input/index.jsx"
+import { HeaderTitle } from "./components/Header/index.jsx"
+import { List } from "./components/List/index.jsx"
+import { Buttons } from "./components/Buttons/index.jsx"
+import "./App.css"
+export const App = () => {
+    return (
+    <div>
+        <HeaderTitle />
+        <List />
+        <Input />
+        <Buttons />
+        </div>
+    )
 }
 
 
---
+++ src/components/Buttons/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+export const Buttons = () => {
+    return (
+         <div className="buttons-style">
+        <button className="task-button">
+        Добавить задание 
+        </button>
+        </div>
+
+    )
+}
